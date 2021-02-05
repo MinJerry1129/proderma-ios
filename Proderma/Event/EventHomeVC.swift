a25002c3559ec0d0390aa3033621641111bb9005
bird <bird@birds-MacBook-Pro.local>
2021-02-05 08:59:41 -0800

    first

---
+++ Proderma/Event/EventHomeVC.swift
@@ -0,0 +1,29 @@
+//
+//  EventHomeVC.swift
+//  Proderma
+//
+//  Created by bird on 2/5/21.
+//
+
+import UIKit
+
+class EventHomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Proderma/SelLangVC.swift
+++ Proderma/SelLangVC.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SelLangVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
